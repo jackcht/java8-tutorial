8d3b86815ed9d907885e3fadd652211875711ff8
Jack Haoting Chen <chen0783@e.ntu.edu.sg>
2020-07-19 23:42:15 -0400

    Lambda1 - add comments

--- src/com/winterbe/java8/samples/lambda/Lambda1.java
+++ src/com/winterbe/java8/samples/lambda/Lambda1.java
@@ -29,21 +29,31 @@ public class Lambda1 {
 
         Collections.sort(names, (a, b) -> b.compareTo(a));
 
-        System.out.println(names);
-
-        names.sort(Collections.reverseOrder());
+        System.out.println(names); //[xenia, peter, mike, anna]
+        /*
+         Returns a comparator that imposes the reverse of the natural ordering on a collection of objects that implement the Comparable interface. 
+         (The natural ordering is the ordering imposed by the objects' own compareTo method.)
+         */
+        names.sort(Collections.reverseOrder());//[xenia, peter, mike, anna]
 
         System.out.println(names);
 
         List<String> names2 = Arrays.asList("peter", null, "anna", "mike", "xenia");
+        /*
+        <String> Comparator<String> java.util.Comparator.nullsLast(Comparator<? super String> comparator)
+        Returns a null-friendly comparator that considers null to be greater than non-null. 
+        When both are null, they are considered equal. 
+        If both are non-null, the specified Comparator is used to determine the order. 
+        If the specified comparator is null, then the returned comparator considers all non-null values to be equal.
+         */
         names2.sort(Comparator.nullsLast(String::compareTo));
-        System.out.println(names2);
+        System.out.println(names2); //[anna, mike, peter, xenia, null]
 
         List<String> names3 = null;
 
         Optional.ofNullable(names3).ifPresent(list -> list.sort(Comparator.naturalOrder()));
 
-        System.out.println(names3);
+        System.out.println(names3); //null
     }
 
 }
