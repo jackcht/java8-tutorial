f17d4b78e3f8e634e52e8e7bbf41450c49e6fd56
Jack Haoting Chen <chen0783@e.ntu.edu.sg>
2021-03-13 12:01:16 -0500

    Update doc

---
+++ src/com/winterbe/test.java
@@ -0,0 +1,20 @@
+String result = map.reduce(1,
+    (key, value) -> {
+        System.out.println("Transform: " + Thread.currentThread().getName());
+        return key + "=" + value;
+    },
+    (s1, s2) -> {
+        System.out.println("Reduce: " + Thread.currentThread().getName());
+        return s1 + ", " + s2;
+    });
+
+System.out.println("Result: " + result);
+
+// Transform: ForkJoinPool.commonPool-worker-2
+// Transform: main
+// Transform: ForkJoinPool.commonPool-worker-3
+// Reduce: ForkJoinPool.commonPool-worker-3
+// Transform: main
+// Reduce: main
+// Reduce: main
+// Result: r2=d2, c3=p0, han=solo, foo=bar
