43f93ac36a6b279d106217d2890464695ee3cea7
Jack Haoting Chen <chen0783@e.ntu.edu.sg>
2021-03-18 22:43:49 -0400

    update Stream8

--- src/com/winterbe/java8/samples/stream/Streams8.java
+++ src/com/winterbe/java8/samples/stream/Streams8.java
@@ -13,27 +13,31 @@ public class Streams8 {
         Arrays.asList("a1", "a2", "a3")
             .stream()
             .findFirst()
-            .ifPresent(System.out::println);
+            .ifPresent(System.out::println);        // a1
 
+        System.out.println();
         Stream.of("a1", "a2", "a3")
             .map(s -> s.substring(1))
             .mapToInt(Integer::parseInt)
-            .max()
-            .ifPresent(System.out::println);
+            .max()          // 3
+            .ifPresent(System.out::println);        // 3
 
+        System.out.println();
         IntStream.range(1, 4)
             .mapToObj(i -> "a" + i)
-            .forEach(System.out::println);
+            .forEach(System.out::println);  // a1, a2, a3
 
+        System.out.println();
         Arrays.stream(new int[] {1, 2, 3})
             .map(n -> 2 * n + 1)
             .average()
-            .ifPresent(System.out::println);
+            .ifPresent(System.out::println);    // 5
 
+        System.out.println();
         Stream.of(1.0, 2.0, 3.0)
             .mapToInt(Double::intValue)
             .mapToObj(i -> "a" + i)
-            .forEach(System.out::println);
+            .forEach(System.out::println);  // a1, a2, a3
 
     }
 }
